66b1cc97d81b3ab5eb92837415d6d8a7cf070c29
rahul <568813855@qq.com>
2017-03-05 13:40:56 +0800

    fix trending analysis

--- network_analysis/stats_network.py
+++ network_analysis/stats_network.py
@@ -112,24 +112,48 @@ def show_node_strength():
     for k, v in avg_strength_map.items():
         print(k, v)
 
-    stock_num = '600016'
+    stock_num = '600051'
     with open('/Users/rahul/tmp/' + stock_num + '_strength.txt', 'w') as out:
         out.write('date strength\n')
         for i in range(0, len(date_ruler)):
             out.write(str(date_ruler[i]) + ' ' + str(strength_map[stock_num][i]) + '\n')
 
-    pass
-
 
 def community_analysis():
     data_dir = '/Users/rahul/tmp/data/graph_data/'
-    filename = data_dir + 'shanghai_2015-07-27.gml'
+    filename = data_dir + 'shanghai_2015-07-28.gml'
 
+    cnt_5, cnt_6, cnt_7, cnt_8, cnt_9, cnt_1_0 = 0, 0, 0, 0, 0, 0
     G = nx.read_gml(filename)
+
+    print(nx.is_directed(G))
+
     threshold = 0.9
     for (u, v) in G.edges():
-        if G[u][v]['weight'] < threshold:
-            G.remove_edge(u, v)
+
+        if G[u][v]['weight'] <= 0.5:
+            cnt_5 += 1
+        elif G[u][v]['weight'] <= 0.6:
+            cnt_6 += 1
+        elif G[u][v]['weight'] <= 0.7:
+            cnt_7 += 1
+        elif G[u][v]['weight'] <= 0.8:
+            cnt_8 += 1
+        elif G[u][v]['weight'] <= 0.9:
+            cnt_9 += 1
+        elif G[u][v]['weight'] <= 1:
+            cnt_1_0 += 1
+
+        # if G[u][v]['weight'] < threshold:
+        #     G.remove_edge(u, v)
+
+
+    print('<= 0.5:', cnt_5, str(cnt_5 / len(G.edges())))
+    print('<= 0.6:', cnt_6, str(cnt_6 / len(G.edges())))
+    print('<= 0.7:', cnt_7, str(cnt_7 / len(G.edges())))
+    print('<= 0.8:', cnt_8, str(cnt_8 / len(G.edges())))
+    print('<= 0.9:', cnt_9, str(cnt_9 / len(G.edges())))
+    print('<= 1.0:', cnt_1_0, str(cnt_1_0 / len(G.edges())))
 
     # nx.write_gml(G, '/Users/rahul/tmp/2015-07-28.0.9.gml')
     partition = community.best_partition(G)
@@ -144,34 +168,49 @@ def community_analysis():
     for k, v in community_cnt.items():
         print(k, len(v))
 
-    sample = ['600051', '601798', '600016', '601398']
-    for stock in sample:
-        for k, v in community_cnt.items():
-            if stock in v:
-                print(stock, k)
 
-    print(community_cnt[1])
+def get_network_avg_strength(begin_date, end_date):
+    result = []
+    date_ruler = list(map(str, get_date_ruler(begin_date, end_date)))
+    for date in date_ruler:
+        strength = []
+        filename = '/Users/rahul/tmp/data/stats/shanghai_strength_' + date + '.txt'
+        with open(filename, 'r') as f:
+            line = f.readline()
+            while line:
+                strength.append(float(line.split(' ')[1]))
+                line = f.readline()
+            print(len(strength))
+            result.append(sum(strength) / len(strength))
+    return result
 
 
 def trending_analysis():
+
+
     data_dir = '/Users/rahul/tmp/data/aligned_data/'
     trading_map, transaction_map = {}, {}
     stocks = os.listdir(data_dir)
     date_ruler = list(map(str, get_date_ruler('2015-01-05', '2015-11-02')))
 
     begin_date, end_date = '2015-07-01', '2015-09-30'
+    # begin_date, end_date = '2015-01-06', '2015-11-02'
     begin_idx, end_idx = date_ruler.index(begin_date), date_ruler.index(end_date)
-
-    # print(begin_idx, end_idx)
-    # print(date_ruler[begin_idx], date_ruler[end_idx])
 
     for i in range(begin_idx, end_idx + 1):
         trading_map[date_ruler[i]] = []
         transaction_map[date_ruler[i]] = []
 
+    # with open('../resources/national_team_investment.csv') as csvfile:
+    #     csv_reader = csv.DictReader(csvfile)
+    #     valid_stock = [row['股票代码'] for row in csv_reader]
+
     for stock in stocks:
         if not stock.startswith('60'):
             continue
+
+        # if not stock[:6] in valid_stock:
+        #     continue
 
         with open(data_dir + stock, 'r') as f:
             csv_reader = csv.DictReader(f)
@@ -185,7 +224,7 @@ def trending_analysis():
                 trading_map[date_ruler[i]].append(int(trading[i]))
                 transaction_map[date_ruler[i]].append(float(transaction[i]))
 
-    trading, transaction, weight_avg, sample_strength, date = [], [], [], [], []
+    trading, transaction, avg_strength, sample_strength, date = [], [], [], [], []
 
     # get everyday's trading volume
     for k, v in trading_map.items():
@@ -195,21 +234,13 @@ def trending_analysis():
     for k, v in transaction_map.items():
         transaction.append(sum(v))
 
-    # get everyday's average weight
-    with open('/Users/rahul/tmp/weight_avg.txt', 'r') as f:
-        csv_reader = csv.DictReader(f, delimiter=' ')
-        weight_avg = [row['weight_avg'] for row in csv_reader]
-
-        f.seek(0)
-        csv_reader = csv.DictReader(f, delimiter=' ')
-        date = [row['date'] for row in csv_reader]
-
-        weight_avg = weight_avg[date.index(begin_date):(date.index(end_date) + 1)]
+    # get everyday's network average strength
+    avg_strength = get_network_avg_strength(begin_date, end_date)
 
     # get stock 600051's everyday's strength
     with open('/Users/rahul/tmp/600051_strength.txt', 'r') as f:
         csv_reader = csv.DictReader(f, delimiter=' ')
-        sample_strength = [row['strength'] for row in csv_reader]
+        sample_strength = [float(row['strength']) for row in csv_reader]
 
         f.seek(0)
         csv_reader = csv.DictReader(f, delimiter=' ')
@@ -221,6 +252,21 @@ def trending_analysis():
     date = date[date.index(begin_date):(date.index(end_date) + 1)]
     date = list(map(lambda x:x[5:], date))
 
+    print((np.corrcoef(sample_strength, avg_strength))[0][1])
+    print((np.corrcoef(trading, transaction))[0][1])
+
+    print((np.corrcoef(sample_strength, transaction))[0][1])
+    print((np.corrcoef(sample_strength, trading))[0][1])
+
+    print((np.corrcoef(avg_strength, transaction))[0][1])
+    print((np.corrcoef(avg_strength, trading))[0][1])
+
+    # save all
+    with open('/Users/rahul/tmp/trending.txt', 'w') as f:
+        f.write(' '.join(['date', 'weight_avg', '600051_weight', 'trading', 'transaction']) + '\n')
+        for i in range(len(date)):
+            f.write(' '.join([date[i], str(avg_strength[i]), str(sample_strength[i]), str(trading[i]), str(transaction[i])]) + '\n')
+
     ind = np.arange(len(date))
     f = '/System/Library/Fonts/STHeiti Medium.ttc'
     prop = fm.FontProperties(fname=f)
@@ -228,7 +274,8 @@ def trending_analysis():
     plt.figure(1)
     plt.subplot(411)
     plt.plot(ind, sample_strength)
-    plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
+    plt.xticks(ind, date, rotation='vertical')
+    # plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
     plt.ylabel('节点600051的强度', fontproperties=prop)
     plt.grid(True)
     plt.annotate('第一次救市', xy=(14, 600), fontproperties=prop)
@@ -237,16 +284,18 @@ def trending_analysis():
     plt.axvspan(xmin=date.index('08-26'), xmax=date.index('09-25'), color='green', alpha=0.2)
 
     plt.subplot(412)
-    plt.plot(ind, weight_avg)
-    plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
-    plt.ylabel('网络平均相关性', fontproperties=prop)
+    plt.plot(ind, avg_strength)
+    plt.xticks(ind, date, rotation='vertical')
+    # plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
+    plt.ylabel('网络平均强度', fontproperties=prop)
     plt.grid(True)
     plt.axvspan(xmin=date.index('07-06'), xmax=date.index('08-14'), color='green', alpha=0.2)
     plt.axvspan(xmin=date.index('08-26'), xmax=date.index('09-25'), color='green', alpha=0.2)
 
     plt.subplot(413)
     plt.plot(ind, transaction, color='red')
-    plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
+    plt.xticks(ind, date, rotation='vertical')
+    # plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
     plt.ylabel('交易额', fontproperties=prop)
     plt.grid(True)
     plt.axvspan(xmin=date.index('07-06'), xmax=date.index('08-14'), color='green', alpha=0.2)
@@ -270,7 +319,6 @@ def main():
     # show_node_strength()
     # community_analysis()
     trending_analysis()
-    pass
 
 
 if __name__ == '__main__':
